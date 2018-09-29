3b5f6dcd8c2d7dc784fbc8b7ed376310efbd1d6f
Tomasz Foster <tomaszfoster@gmail.com>
2018-09-29 11:15:00 -0400

    Updated test file

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-import cv2
+import cv2 as cv
 
-img = cv2.imread('flubba1.jpg')
+IMG = cv.imread('flubba1.jpg')
 
-cv2.imwrite('flubba.out.jpg',img)
+# do something to the image
+# img[:,:,0] = 0
+
+# save the image
+SIZE = 500
+REFLECTION = cv.copyMakeBorder(IMG, SIZE, SIZE, SIZE, SIZE, cv.BORDER_REFLECT_101)
+cv.imwrite('flubba.out.jpg', REFLECTION)
